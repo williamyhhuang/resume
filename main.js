66f183fadb9e5b20f919bfc2718b25262848884a
williamyhhuang <yhhuang1992@gmail.com>
2019-09-05 00:29:19 +0800

    update info
    update info of introduce and title

--- main.js
+++ main.js
@@ -1,5 +1,49 @@
-document.getElementById('introduce').addEventListener('click', loadTxt);
-function loadTxt() {
+/*
+var title_list = ['Let me introduce myself',
+                  'Why do I want to be a backend engineer?',
+                  'How do I learn by myself?',
+                  'what is my favorite subject in school?',
+                  'What\'s the most chanllening thing in my life?',
+                  'Here\'s my introduction video'
+    
+];
+*/
+
+var title = document.querySelector('.title');
+var introduce = document.querySelector('.introduce');
+var why = document.querySelector('.why');
+var how = document.querySelector('.how');
+var favorite = document.querySelector('.favorite');
+var challenge = document.querySelector('.challenge');
+var career = document.querySelector('.career');
+var video = document.querySelector('.video');
+
+
+/*
+introduce.onclick = function introduce() {
+      title.textContent = "Let me introduce myself";
+}
+
+why.onclick = function why_title() {
+      title.textContent = 'Why do I want to be a backend engineer?';
+}
+how.onclick = function how() {
+      title.textContent = 'How do I learn by myself?';
+}
+favorite.onclick = function favorite() {
+      title.textContent = 'what is my favorite subject in school?';
+}
+challenge.onclick = function chanllenge() {
+      title.textContent = 'What\'s the most chanllening thing in my life?';
+}
+video.onclick = function video() {
+      title.textContent = 'Here\'s my introduction video';
+}
+*/
+
+document.getElementById('introduce').addEventListener('click', loadTxt_introduce);
+function loadTxt_introduce() {
+    title.textContent = "Let me introduce myself";
     fetch('info/introduce.txt')
     .then(function(response){
         return response.text();
@@ -13,8 +57,82 @@ function loadTxt() {
     })
 }
 
-var why_result = document.querySelector('.why_result');
-var why = document.querySelector('.why');
-why.onclick = function why() {
-      why_result.textContent = 'why myself'
+document.getElementById('why').addEventListener('click', loadTxt_why);
+function loadTxt_why() {
+    title.textContent = 'Why do I want to be a backend engineer?';
+    fetch('info/why.txt')
+    .then(function(response){
+        return response.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('result').innerHTML = data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+document.getElementById('how').addEventListener('click', loadTxt_how);
+function loadTxt_how() {
+    title.textContent = 'How do I learn by myself?';
+    fetch('info/how.txt')
+    .then(function(response){
+        return response.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('result').innerHTML = data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+document.getElementById('favorite').addEventListener('click', loadTxt_favorite);
+function loadTxt_favorite() {
+    title.textContent = 'What\'s my favorite subject in school?';
+    fetch('info/favorite.txt')
+    .then(function(response){
+        return response.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('result').innerHTML = data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+document.getElementById('challenge').addEventListener('click', loadTxt_challenge);
+function loadTxt_challenge() {
+    title.textContent = 'What\'s the most chanllening thing in my life?';
+    fetch('info/challenge.txt')
+    .then(function(response){
+        return response.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('result').innerHTML = data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+document.getElementById('career').addEventListener('click', loadTxt_career);
+function loadTxt_career() {
+    title.textContent = 'Here is my career plan';
+    fetch('info/career.txt')
+    .then(function(response){
+        return response.text();
+    })
+    .then(function(data){
+        console.log(data);
+        document.getElementById('result').innerHTML = data;
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 }
